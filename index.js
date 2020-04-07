ab4fcb34221cf238736fdfe8458a6d71376f5100
aricjean <51228393+aricjean@users.noreply.github.com>
2020-04-07 14:44:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const Koa = require('koa');
 
 // 创建一个Koa对象表示web app本身:
 const app = new Koa();
-
+var port = process.env.PORT ? process.env.PORT : 12355;
 // 对于任何请求，app将调用该异步函数处理请求：
 app.use(async (ctx, next) => {
     await next();
     ctx.response.type = 'text/html';
-    ctx.response.body = '<h1>Hello, koa2!</h1>port: ' + process.env.port;
+    ctx.response.body = '<h1>Hello, koa2!</h1>port: ' + port;
 });
-var port = process.env.PORT ? process.env.PORT : 12355;
+
 app.listen(port, () => {
   console.log("server started at port:", port);
 })
