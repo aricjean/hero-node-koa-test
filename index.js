9ed37ca66295c667fbc639ee324b2d506ec797b1
ranai <ranai@cqysxx.com>
2020-04-07 19:42:33 +0800

    remove monge

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const Koa = require('koa');
 const app = new Koa();
 var port = process.env.PORT ? process.env.PORT : 12355;
 const MongoClient = require('mongodb').MongoClient;
-function mongo() {
+async function mongo() {
+return new Promise((resolve, reject) => {
 
 const uri = "mongodb+srv://admin:zxcasdqwe@cluster0-zvimr.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
@@ -19,13 +20,17 @@ client.connect(err => {
 	`;
 });
 
+});
 }
 // 对于任何请求，app将调用该异步函数处理请求：
 app.use(async (ctx, next) => {
     await next();
     ctx.response.type = 'text/html';
     // ctx.response.body = '<h1>Hello, koa2!</h1>port: ' + port;
-	mongo();
+	// mongo();
+	setTimeout(() => {
+		ctx.response.body = 'hello world';
+	}, 0);
     // ctx.response.body = `
     //  <h1>Hello, koa2!</h1>
     //  <p>server started at port: ${port}</p>
