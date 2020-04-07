b62e0be0ebdcb2158f02e5cf877c3babe14839e9
ranai <ranai@cqysxx.com>
2020-04-07 14:26:16 +0800

    chg

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app.use(async (ctx, next) => {
     ctx.response.type = 'text/html';
     ctx.response.body = '<h1>Hello, koa2!</h1>port: ' + process.env.port;
 });
-app.listen(process.env.port, () => {
+app.listen(process.env.port ? process.env.port : 12355, () => {
   console.log("server started at post:", process.env.port);
 })
